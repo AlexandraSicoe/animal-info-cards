f44949e91eaafc0793417efe09ff3fd61ab7713e
Lex <sicoealexandra97@gmail.com>
2023-07-11 15:14:28 +0300

    updated animal cards page

--- src/components/AnimalCards.js
+++ src/components/AnimalCards.js
@@ -1,6 +1,9 @@
 import Grid from "@mui/joy/Grid";
 import Typography from "@mui/joy/Typography";
 import backgroundImage from "../images/bg1.jpg";
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
 
 const AnimalCards = () => {
   return (
@@ -16,32 +19,70 @@ const AnimalCards = () => {
         backgroundPosition: "center",
       }}
     >
-      <Grid
-        container
-        justifyContent="center"
-        direction="column"
-        alignItems="center"
-        height="100%"
-      >
-        <Typography
-          level="h1"
-          maxWidth={800}
-          textAlign="center"
-          style={{ color: "#dcae96", fontFamily: "Montserrat" }}
-        >
-          Discover the Marvels of the Animal Kingdom
-        </Typography>
-        <Typography
-          level="h3"
-          maxWidth={900}
-          lineHeight="md"
-          style={{
-            color: "#ffefd5",
-            marginTop: "25px",
-            fontFamily: "Montserrat",
-            textIndent: "10%",
-          }}
-        ></Typography>
+      <Grid container justifyContent="center" alignItems="center" height="100%">
+        <Grid m={2}>
+          <Card component="li" sx={{ minWidth: 300, minHeight: 400 }}>
+            <CardCover>
+              <img
+                src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+                srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
+                alt=""
+              />
+            </CardCover>
+            <CardContent>
+              <Typography
+                level="body1"
+                fontWeight="lg"
+                textColor="#fff"
+                sx={{ m: "auto" }}
+              >
+                More details...
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid m={2}>
+          <Card component="li" sx={{ minWidth: 300, minHeight: 400 }}>
+            <CardCover>
+              <img
+                src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+                srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
+                alt=""
+              />
+            </CardCover>
+            <CardContent>
+              <Typography
+                level="body1"
+                fontWeight="lg"
+                textColor="#fff"
+                sx={{ m: "auto" }}
+              >
+                More details...
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid m={2}>
+          <Card component="li" sx={{ minWidth: 300, minHeight: 400 }}>
+            <CardCover>
+              <img
+                src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+                srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
+                alt=""
+              />
+            </CardCover>
+            <CardContent>
+              <Typography
+                level="body1"
+                fontWeight="lg"
+                textColor="#fff"
+                sx={{ m: "auto" }}
+              >
+                More details...
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </div>
   );
